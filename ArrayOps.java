8c69ecc49106257cc89ae8c299e9cdefd1974a73
Igbathscaller <Igbathscaller@gmail.com>
2020-10-23 13:12:10 -0400

    sum

--- ArrayOps.java
+++ ArrayOps.java
@@ -1,3 +1,9 @@
 public class ArrayOps{
-    
+    public static int sum(int[] arr){
+        int a = 0;
+        for(int i = 0; i< arr.length; i++){
+            a++;
+        }
+        return a;   
+        }
 }
