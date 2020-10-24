6b1f1e394a2245761f4121b9cf402cd082fc3d9d
Igbathscaller <Igbathscaller@gmail.com>
2020-10-24 00:06:54 -0400

    fixed ++ instead of +=arr[i]

--- ArrayOps.java
+++ ArrayOps.java
@@ -2,7 +2,7 @@ public class ArrayOps{
     public static int sum(int[] arr){
         int a = 0;
         for(int i = 0; i< arr.length; i++){
-            a++;
+            a+=arr[i];
         }
         return a;   
         }
--- Tester.java
+++ Tester.java
@@ -1,3 +1,5 @@
+import java.util.*;
+
 public class Tester{
     public static void main(String[]args){
         int[][] A = {   {  1,  0, 12, -1 },
